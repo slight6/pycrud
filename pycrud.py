cd18b9b8bf0d38269aed9b9a3fe562b88c83ffc0
jason consiglio <jason@27pro.com>
2023-10-21 05:21:43 -0400

    connected, rough but opperations are working

--- pycrud.py
+++ pycrud.py
@@ -16,3 +16,53 @@ try:
 except Exception as e:
     print(e)
 
+# Getting a Database
+
+db = client['pycrud']
+print(f'Your database is: {db}')
+
+# Creating a new collection
+
+db.create_collection('users')
+print(f'Your collections are: {db.list_collection_names()}')
+
+# Getting a Collection
+
+users = db['users']
+print(f'Your collection is: {users}')
+
+# Inserting a Document
+
+users.insert_one({'name': 'John Doe', 'age': 25})
+print(f'You inserted a document: {users.find_one()}')
+
+# Inserting Multiple Documents
+
+users.insert_many([
+    {'name': 'Jane Doe', 'age': 25},
+    {'name': 'John Smith', 'age': 30},
+    {'name': 'Jane Smith', 'age': 30}
+])
+
+# Finding Documents
+
+users.find_one({'name': 'John Doe'})
+
+# Finding All Documents
+
+users.find()
+
+# Finding Documents with a Query Filter
+
+output = users.find({'age': 30})
+print(f'We found the users with the ages of 30: {output}')
+
+# Updating Documents
+
+users.update_one({'name': 'John Doe'}, {'$set': {'age': 26}})
+users.update_many({'age': 30}, {'$set': {'age': 31}})
+
+# Deleting Collections
+
+users.delete_many({'age': 31, 'age': 26, 'age': 25})
+
